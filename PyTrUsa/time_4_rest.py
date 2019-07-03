153b8473e15e3c5f68a84d037b14b83f03cfd149
Usama Iqbal <mohamedusama881@gmail.com>
2019-07-03 09:00:15 +0530

    Add files via upload
    Interval Reminder

---
+++ PyTrUsa/time_4_rest.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+# no of times you need to take a break
+count = 2
+while count > 0:
+    # stop the execution for 10sec
+    time.sleep(44)
+    webbrowser.open("https://www.youtube.com/watch?v=vNLZAmLqibo")
+    count -= 1
