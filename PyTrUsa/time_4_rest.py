423b8fdf658d0add8dc8e143a0588063dcd8a688
Usama Iqbal <mohamedusama881@gmail.com>
2019-07-03 16:55:46 +0530

    Update time_4_rest.py

--- PyTrUsa/time_4_rest.py
+++ PyTrUsa/time_4_rest.py
@@ -1,10 +1,14 @@
 import webbrowser
 import time
 
+#App/script-which Remind user to take necessary interval time to time 
+
 # no of times you need to take a break
-count = 2
+
+count = 3
+
 while count > 0:
     # stop the execution for 10sec
-    time.sleep(44)
+    time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=vNLZAmLqibo")
     count -= 1
