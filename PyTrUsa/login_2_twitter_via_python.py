4b48d5632aa1194ffb2a7e3c1be3cdaad39d1416
Usama Iqbal <mohamedusama881@gmail.com>
2019-07-04 23:16:23 +0530

    Update login_2_twitter_via_python.py

--- PyTrUsa/login_2_twitter_via_python.py
+++ PyTrUsa/login_2_twitter_via_python.py
@@ -4,8 +4,9 @@ from getpass import getpass
 usr = input("Enter Yo Username or Email: ")
 pwd = getpass("Yo Passwrord: ")
 
-#Download and place the chrome driver.exe to a location
-driver = webdriver.Chrome(executable_path=r"C:\Users\uiqbal\Desktop\chromedriver.exe")
+# Download and place the chrome driver.exe to a location
+driver = webdriver.Chrome(
+    executable_path=r"C:\Users\Young_Blood\Desktop\Python Training\python-virtusa\new_try\chromedriver.exe")
 driver.get('https://twitter.com/login')
 
 usr_box = driver.find_element_by_class_name('js-username-field')
